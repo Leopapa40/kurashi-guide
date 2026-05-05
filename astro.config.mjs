7695bd4ffa5192667655b7876f7a43ea09a32fec
Leopapa40 <Leopapa40@users.noreply.github.com>
2026-05-06 06:44:33 +0900

    chore: ドメインをkurashiguide.orgに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kurashiguide.com',
+  site: 'https://kurashiguide.org',
   trailingSlash: 'always',
   integrations: [
     sitemap({
